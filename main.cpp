70bdcb171eb1e53759fa72c8ecb97b523b52d642
Dustin Wendt <dustinwendt@ku.edu>
2017-04-11 23:11:22 -0500

    Added comments

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int set_assoc;
 
 int main(int argc, char** argv)
 {
+	//Check if arguments are valid
 	if(argc != 3) {
 		printf("Usage: ./cache_sim <set_assoc (1 or 2)> <binary_file_name>\n");
 		return 1;
@@ -20,6 +21,7 @@ int main(int argc, char** argv)
 
 	set_assoc = argv[1][0] - '0';
 
+	//Program only allows for 1 and 2-way set associative
 	if(set_assoc != 1 && set_assoc != 2) {
 		printf("Err: supported set associativity includes: 1, 2\n");
 		return 1;
@@ -28,10 +30,12 @@ int main(int argc, char** argv)
 	char* file_name = argv[2];
 	FILE *input_file = fopen(file_name, "rb");
 
+	//allocate space for entries on the heap. Sizeof(char) is a null terminator, because malloc is a C function
 	uint32_t *data = (uint32_t *) malloc((sizeof(uint32_t) * NUM_ENTRIES) + sizeof(char));
 	fread(data, (NUM_ENTRIES * 4), 1, input_file);
 	fclose(input_file);
 
+	//print out data read in
 	for(int i = 0; i < NUM_ENTRIES; ++i) {
 		printf("0x%08x\t", data[i]);
 		if(i % 4 == 0) printf("\n");
